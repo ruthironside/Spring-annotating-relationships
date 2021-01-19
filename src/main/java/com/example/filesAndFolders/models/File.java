5e7606c4374b865c96a7928be149f1f444eb2066
Ruth Ironside <ruthironside@live.com>
2021-01-19 16:27:32 +0000

    File class set up

---
+++ src/main/java/com/example/filesAndFolders/models/File.java
@@ -0,0 +1,81 @@
+package com.example.filesAndFolders.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
+public class File {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "extension")
+    private String extension;
+
+    @Column(name = "size")
+    private int size;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnoreProperties({"files"})
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+
+    public File(String name, String extension, int size, Folder folder) {
+        this.name = name;
+        this.extension = extension;
+        this.size = size;
+        this.folder = folder;
+    }
+
+    public File() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public void setExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+
+}
---
+++ src/main/java/com/example/filesAndFolders/models/User.java
@@ -0,0 +1,4 @@
+package com.example.filesAndFolders.models;
+
+public class User {
+}
